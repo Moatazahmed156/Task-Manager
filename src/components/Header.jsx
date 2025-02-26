e2ca814956a37e7d282c1ada2c889a53b637d77e
Moataz Ahmed <146819341+Moatazahmed156@users.noreply.github.com>
2025-02-26 17:54:56 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Head({ nexthead }) {
   return (
     <div>
       <div id="mainhead" className="flex flex-col gap-8 my-8">
-        <h1 className="text-white text-3xl font-bold">To Do List</h1>
+        <h1 className="text-white text-4xl font-bold">To Do List</h1>
         <button
           className="bg-[#2972ee] text-white rounded-2xl px-4 py-2"
           onClick={nexthead}
