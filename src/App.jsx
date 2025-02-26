d1b228c684320699db7b4107fd52065c1bf874c1
Moataz Ahmed <146819341+Moatazahmed156@users.noreply.github.com>
2025-02-26 18:12:47 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,23 @@ import Main from "./components/Main";
 import Head from "./components/Header";
 import Sechead from "./components/Secondhead";
 function App() {
-  const [showhead, setShowhead] = useState(false);
+  const [showhead, setShowhead] = useState(<Head nexthead={nexthead} />);
   function nexthead() {
-    setShowhead(true);
+    setShowhead(<Sechead addTask={addTask} prevhead={prevhead} />);
   }
   function prevhead() {
-    setShowhead(false);
+    setShowhead(<Head nexthead={nexthead} />);
   }
   const [tasks, setTasks] = useState([]);
   function addTask(newTask) {
     setTasks([...tasks, newTask]);
-    setShowhead(false);
+    setShowhead(<Head nexthead={nexthead} />);
   }
 
   return (
     <div className="w-[650px] h-[95%] max-md:w-[350px]">
       <div className="bg-[#171a2f] w-[100%] h-[30%] justify-center  flex flex-col items-center max-md:h-[50%]">
-        {showhead ? (
-          <Sechead addTask={addTask} prevhead={prevhead} />
-        ) : (
-          <Head nexthead={nexthead} />
-        )}
+        {showhead}
       </div>
       <Main tasks={tasks} />
     </div>
